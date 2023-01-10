e4c00d2d44b66050ce2270292f582ad7ea3077ae
KozAAAAA <mateusz-koza@outlook.com>
2023-01-10 16:38:16 +0100

    my_changes.py

--- detect.py
+++ detect.py
@@ -7,6 +7,7 @@ import cv2
 from tqdm import tqdm
 
 import numpy as np
+from my_func_and_ranges import *
 
 
 def detect(img_path: str) -> Dict[str, int]:
@@ -28,51 +29,20 @@ def detect(img_path: str) -> Dict[str, int]:
 
     print(img_path)
 
-    blurred_img = cv2.GaussianBlur(img, (41, 41), 0)
-    hsv_img = cv2.cvtColor(blurred_img, cv2.COLOR_BGR2HSV)
-
-    lower_red_mask = np.array([173, 91, 100])
-    upper_red_mask =np.array([179, 255, 255])
-    lower_purple_mask = np.array([140,0,0])
-    upper_purple_mask =np.array([173,255,103])
-
-    lower_yellow_mask = np.array([10,201,119])
-    upper_yellow_mask =np.array([30,255,255])
-
-    lower_green_mask = np.array([31,188,119])
-    upper_green_mask =np.array([52,255,255])
-
-    red_mask = cv2.inRange(hsv_img, lower_red_mask, upper_red_mask)
-    purple_mask = cv2.inRange(hsv_img, lower_purple_mask, upper_purple_mask)
-    yellow_mask = cv2.inRange(hsv_img, lower_yellow_mask, upper_yellow_mask)
-    green_mask = cv2.inRange(hsv_img, lower_green_mask, upper_green_mask)
-
-    kernel = np.ones((5,5),np.uint8)
-
-    red_mask = cv2.dilate(red_mask, kernel , iterations=3)
-    purple_mask = cv2.dilate(purple_mask, kernel , iterations=3)
-    yellow_mask = cv2.dilate(yellow_mask, kernel , iterations=3)
-    green_mask = cv2.dilate(green_mask, kernel , iterations=3)
-
-    red_opening = cv2.morphologyEx(red_mask, cv2.MORPH_OPEN, kernel, iterations=2)
-    purple_opening = cv2.morphologyEx(purple_mask, cv2.MORPH_OPEN, kernel, iterations=2)
-    yellow_opening = cv2.morphologyEx(yellow_mask, cv2.MORPH_OPEN, kernel, iterations=2)
-    green_opening = cv2.morphologyEx(green_mask, cv2.MORPH_OPEN, kernel, iterations=2)
-
-    red_contours, _ = cv2.findContours(red_opening,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    purple_contours, _ = cv2.findContours(purple_opening,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    yellow_contours, _ = cv2.findContours(yellow_opening,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    green_contours, _ = cv2.findContours(green_opening,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-    # print("red = " + str(len(red_contours)))
-    # print("purple = " + str(len(purple_contours)))
-    # print("yellow = " + str(len(yellow_contours)))
-    # print("green = " + str(len(green_contours)))
+    red_count, red_display = get_number_of_objects_in_range(img, lower_red, upper_red)
+    purple_count, purple_display = get_number_of_objects_in_range(img, lower_purple, upper_purple)
+    yellow_count, yellow_display = get_number_of_objects_in_range(img, lower_yellow, upper_yellow)
+    green_count, green_display = get_number_of_objects_in_range(img, lower_green, upper_green)
     
-    red = len(red_contours)
-    yellow = len(yellow_contours)
-    green = len(green_contours)
-    purple = len(purple_contours)
+    # print("red = " + str(red_count))
+    # print("purple = " + str(purple_count))
+    # print("yellow = " + str(yellow_count))
+    # print("green = " + str(green_count))
+    
+    red = red_count
+    yellow = yellow_count
+    green = green_count
+    purple = purple_count
 
     return {'red': red, 'yellow': yellow, 'green': green, 'purple': purple}
 
